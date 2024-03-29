379585f7878c00eaf19776bbf97c459b2700b496
rolisingh <rolsi1803@gmail.com>
2024-03-29 19:22:47 +0530

    date anad time

--- src/index.js
+++ src/index.js
@@ -8,23 +8,51 @@ import ReactDOM from 'react-dom';
 
 
 
+
+
+//ASSIGNMENT 
+
+// const name="roli";
+// const currdate = new Date().toLocaleDateString();
+// const currtime = new Date().toLocaleTimeString();
+
+// ReactDOM.render(
+
+//   <>
+
+//     <h1>Hello, My name is {name}</h1>
+//     <p>todays date is {currdate}</p>
+//     <p>current Time is {currtime}</p>
+//   </>,
+//   document.getElementById("root")
+// );
+
+
+
+
+
+//template literals
+
+// const name="rooli ";
+// const surname="singh"
+// ReactDOM.render(
+//   <h1>{ `my name is ${name} ${surname}`  }</h1>,
+//   document.getElementById("root")
+// )
+
+
+
+
 // JSX Expression {}  if you want to write js code in html tag just write {}
 
-const name="Roli singh";
-ReactDOM.render(
-  <>
-  <h1>My name is {name}</h1>
-  <p>two plus two is equal to {2+2}</p>
-  </>,
-  document.getElementById("root")
-)
-
-
-
-
-
-
-
+// const name="Roli singh";
+// ReactDOM.render(
+//   <>
+//   <h1>My name is {name}</h1>
+//   <p>two plus two is equal to {2+2}</p>
+//   </>,
+//   document.getElementById("root")
+// )
 
 
 
