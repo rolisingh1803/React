3d1b2842d54865438bc09e98568e5e466245aab9
rolisingh <rolsi1803@gmail.com>
2024-03-29 18:15:44 +0530

    jsx expression

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+
+// JSX Expression {}  if you want to write js code in html tag just write {}
+
+const name="Roli singh";
+ReactDOM.render(
+  <>
+  <h1>My name is {name}</h1>
+  <p>two plus two is equal to {2+2}</p>
+  </>,
+  document.getElementById("root")
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//multiple elements 
+
+// ReactDOM.render(
+//   <>
+
+//   <h1>hello world!</h1>
+//     <p>Top 2 Web Series</p>
+
+//     <ol>
+//       <li>Asur</li>
+//       <li>Scam 1992</li>
+//       <li>india</li>
+//     </ol>
+//     </>,
+//     document.getElementById("root")
+// )
+
+
+
+
